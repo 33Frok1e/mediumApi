aa827fa0d004f1ef3e46c7c4ed47b6ef5fbd2a7f
33Frok1e <mratikrishna@gmail.com>
2025-05-18 12:17:35 +0530

    service Account key added

--- server.js
+++ server.js
@@ -9,14 +9,26 @@ import admin from "firebase-admin";
 // import serviceAccountKey from "./medium-firebase-adminsdk.json" assert { type: "json" };
 import { getAuth } from "firebase-admin/auth";
 // import aws from "aws-sdk";
-import { readFile } from 'fs/promises';
-const serviceAccountKey = JSON.parse(await readFile(new URL('./medium-firebase-adminsdk.json', import.meta.url)));
 
 //schema below
 import User from "./Schema/User.js";
 import Blog from "./Schema/Blog.js";
 import Notification from "./Schema/Notification.js";
 import Comment from "./Schema/Comment.js";
+
+const serviceAccountKey = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
 
 const server = express();
 let PORT = 3000;
